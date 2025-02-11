0a81fa0272ddb068f1c2674f93b50d7fdaee87a3
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-11 14:37:18 -0500

    Did more of the lesson.

--- Topic 4 Lesson/Program.cs
+++ Topic 4 Lesson/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Topic_4_Lesson
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Topic_4_Lesson
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String userName;
+            int age, discount;
+            double price;
+
+            discount = 50;
+
+
+            Console.WriteLine("Enter username");
+            userName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Your username is: " + userName + " ... Press Enter to Continue.");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Your age is: " + age);
+            Console.WriteLine();
+            Console.WriteLine("What is the Price?");
+            Double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine("The Price is " + price.ToString("C"));
+
         }
     }
 }
